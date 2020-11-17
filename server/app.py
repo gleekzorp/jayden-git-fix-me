ef0261379e3618b9bd5650c9e7f3e039a2d13d38
Jayden Dyer <jaydendyer@Jaydens-Air.domain>
2020-11-17 15:59:49 -0700

    changed get all books to an array and changed titles to title

--- server/app.py
+++ server/app.py
@@ -43,7 +43,7 @@ def add_book():
     genre = request.json["genre"]
     star_rating = request.json["star_rating"]
     book_read = request.json["book_read"]
-    new_book = Book(titles=title, author=author, url=url,
+    new_book = Book(title=title, author=author, url=url,
                     genre=genre, star_rating=star_rating, book_read=book_read)
     db.session.add(new_book)
     db.session.commit()
@@ -59,7 +59,7 @@ def get_books():
     else:
         all_books = Book.query.all()
     result = books_schema.dump(all_books)
-    return book_schema.jsonify(result)
+    return books_schema.jsonify(result)
 
 
 # PUT/PATCH by ID
